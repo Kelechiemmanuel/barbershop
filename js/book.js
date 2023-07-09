6948adad9e56858b70880c91b2ae558ce87d8207
kelechi <ekelechi413@gmail.com>
2023-07-08 19:44:42 -0700

    done

--- js/book.js
+++ js/book.js
@@ -1,53 +1,53 @@
-
 function booking() {
     document.querySelector('.middle-side-booking').style.display = 'block';
+    document.querySelector('.middle-side-service').style.display = 'none';
     document.querySelector('.middle-side-staff').style.display = 'none';
-    document.querySelector('.middle-side-service').style.display = 'none'; 
 }
 
 function staffs() {
-    document.querySelector('.middle-side-booking').style.display = 'none';
     document.querySelector('.middle-side-staff').style.display = 'block';
-    document.querySelector('.middle-side-service').style.display = 'none'; 
-}
-function services() {
     document.querySelector('.middle-side-booking').style.display = 'none';
+    document.querySelector('.middle-side-service').style.display = 'none';
+
+}
+
+function services() {
+    document.querySelector('.middle-side-service').style.display = 'block';
     document.querySelector('.middle-side-staff').style.display = 'none';
-    document.querySelector('.middle-side-service').style.display = 'block'; 
-    document.querySelector('.headline').style.display = 'none';
+    document.querySelector('.middle-side-booking').style.display = 'none';
+
 }
 
 function openStaff() {
+    document.querySelector('.worker').style.display = 'block';
+    document.querySelector('.quick').style.display = 'none';
+    document.querySelector('.book-con').style.display = 'none';
+}
+
+function myFunction() {
+    document.getElementById('demo1').style.display = 'block';
+    document.getElementById('demo2').style.display = 'none';
+    document.getElementById('demo3').style.display = 'none';
     document.querySelector('.staff-around').style.display = 'block';
-    document.querySelector('.book-con').style.display = 'none';   
-    document.querySelector('.headline').style.display = 'none';
+    
+}
+function myStaff() {
+    document.getElementById('demo1').style.display = 'none';
+    document.getElementById('demo2').style.display = 'block';
+    document.getElementById('demo3').style.display = 'none';
+    document.querySelector('.staff-around').style.display = 'block';
+    
+}
+function myWork() {
+    document.getElementById('demo1').style.display = 'none';
+    document.getElementById('demo2').style.display = 'none';
+    document.getElementById('demo3').style.display = 'block';
+    document.querySelector('.staff-around').style.display = 'block';
 }
 
-function backBtn() {
+function goBack() {
     document.querySelector('.book-con').style.display = 'block';
-    document.querySelector('.staff-around').style.display = 'none';
-    document.querySelector('.headline').style.display = 'flex';
-}
-
-function staffOpen() {
-    document.querySelector('.date-time').style.display = 'block';
-    document.querySelector('.staff-all2').style.display = 'none';
-    document.querySelector('.staff-all3').style.display = 'none';
-    document.querySelector('.staff-all4').style.display = 'none';
-    document.querySelector('.headline').style.display = 'none';
-    document.querySelector('.back-arrow').style.display = 'none';
-}
-
-function backBtnz() {
-    document.querySelector('.staff-all2').style.display = 'flex';
-    document.querySelector('.staff-all3').style.display = 'flex';
-    document.querySelector('.staff-all4').style.display = 'flex';
-    document.querySelector('.date-time').style.display = 'none';
-    document.querySelector('.back-arrow').style.display = 'flex';
+    document.querySelector('.worker').style.display = 'none';
 }
 
 
-function nextBtn() {
-    document.querySelector('.contact-form').style.display = 'flex';
-    document.querySelector('.date-time').style.display = 'none';
-}
